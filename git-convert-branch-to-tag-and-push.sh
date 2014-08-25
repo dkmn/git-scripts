4e7b85ece7652d7b4316c6bf95ef797d516cf9dc
David K. Kim <david@radardoc.net>
2014-08-25 11:54:41 -0700

    Cleaned up, added comments. Also removed --tags modifier as the semantics were to push all tags. After deleting the branch, the specifier of the branch and tag names are no longer ambiguous and so specifying it refers to the tag (see new line 17).

--- git-convert-branch-to-tag-and-push.sh
+++ git-convert-branch-to-tag-and-push.sh
@@ -4,11 +4,17 @@
 # then delete branch
 # usage: branch_to_tag $BRANCHNAME
 
+# get the specific SHA hash of the commit to which the branch points
 REF1=`git show-ref --heads --hash "$1"`
 echo $1 $REF1
+
+# create a tag at this specific commit
 git tag $1 $REF1
+# delete the branch pointer
 git branch -D $1
 
-#git push origin :$1
+# iterate through all remotes and delete branch
 for remote in $(git remote); do git push $remote :$1; done
-for remote in $(git remote); do git push --tags $remote $1; done
+# iterate through all remotes and push tag (reference is now unique as the
+# branch is gone)
+for remote in $(git remote); do git push $remote $1; done
--- git-convert-branch-to-tag.sh
+++ git-convert-branch-to-tag.sh
@@ -4,10 +4,11 @@
 # then delete branch
 # usage: branch_to_tag $BRANCHNAME
 
+# get the specific SHA hash of the commit to which the branch points
 REF1=`git show-ref --heads --hash "$1"`
 echo $1 $REF1
-git tag $1 $REF1
-git branch -D $1
 
-#git push origin :$1
- 
+# create a tag at this specific commit
+git tag $1 $REF1
+# delete the branch pointer
+git branch -D $1
